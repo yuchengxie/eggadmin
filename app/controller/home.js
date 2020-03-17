f48b138bfed1862ff2d1ebabe8d70bf180494974
yucheng <2043232616@qq.com>
2020-03-17 20:29:49 +0800

    mac

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,8 +4,9 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
+    this.ctx.body={
+      msg:'welcome hzfds api'
+    }
   }
 }
 
--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,19 @@
-'use strict';
-
-/** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose'
 };
+
+exports.cors = {
+  enable: true,
+  package: 'egg-cors'
+};
+
+exports.jwt = {
+  enable: true,
+  package: "egg-jwt"
+};
+
+exports.redis = {
+  enable: true,
+  package: 'egg-redis'
+}
